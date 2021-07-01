eb819be1226ce4fb39dfa947a2f9cac1301a3cdd
Artur Vasyliev <princeartik@gmail.com>
2021-07-01 15:36:49 +0300

    Implemented simple goal list web app with Vanilla JS

---
+++ GoalListFromJSToVue/app.js
@@ -0,0 +1,13 @@
+const addGoalButton = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('ul');
+
+
+
+addGoalButton.addEventListener('click', function(){
+    const enteredValue = inputEl.value;
+    const listItemEl = document.createElement('li');
+    listItemEl.textContent = enteredValue;
+    listEl.appendChild(listItemEl);
+    inputEl.value = '';
+});
