ac78a2e1bc0f347ba836f4f9f7666813c7b2a743
Artur Vasyliev <princeartik@gmail.com>
2021-07-02 16:31:17 +0300

    Methods in Vue

--- basic-app-1/app.js
+++ basic-app-1/app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
             testMessage: 'This is my test message',
             vueLink: 'https://vuejs.org/'
         }
+    },
+    methods: {
+        testOutput() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 'Learn Vue!';
+            } else {
+                return 'Master Vue!';
+            }
+        }
     }
 });
 app.mount('#user-goal');
