3944c5f9690736f9fba6905087b3e22c8157241e
Artur Vasyliev <princeartik@gmail.com>
2021-07-07 20:49:01 +0300

    Updating health bars

--- the-vuetcher/app.js
+++ the-vuetcher/app.js
@@ -1,26 +1,34 @@
 const app = Vue.createApp({
   data() {
-      return {
-          playerHealth: 100,
-          monsterHealth: 100
-      }
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  computed: {
+    monsterBarStyle() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyle() {
+      return {width: this.playerHealth + '%'};
+    },
   },
   methods: {
-      attackMonster() {
-          const attackValue = getRandomValue(5, 12);
-          this.monsterHealth -= attackValue;
-          this.attackPlayer();
-      },
-      attackPlayer() {
-        const attackValue = getRandomValue(8, 15);
-        this.playerHealth -= attackValue;
-    }
-  }
+    attackMonster() {
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
+  },
 });
 
 app.mount("#game");
 
 function getRandomValue(min, max) {
-    // getting random number from min to max
-    return Math.floor(Math.random() * (max - min)) + min;
+  // getting random number from min to max
+  return Math.floor(Math.random() * (max - min)) + min;
 }
