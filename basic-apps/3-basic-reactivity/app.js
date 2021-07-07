2cba3984a54a0068598a4b91c8c58761cc54c62f
Artur Vasyliev <princeartik@gmail.com>
2021-07-07 09:55:56 +0300

    Computed properties

--- basic-apps/3-basic-reactivity/app.js
+++ basic-apps/3-basic-reactivity/app.js
@@ -5,7 +5,23 @@ const app = Vue.createApp({
       name: ''
     };
   },
+  computed: {
+    // computed properties are used to output some data which will be recalculated
+    // every time the dependencies of properties are changed
+    fullname() {
+      // Vue saves dependencies of computed proeprties (e.g. this.name)
+      // and recalculates computed property only when dependencies 
+      // are changed to improve the performance
+      console.log('fullname call');
+      if(this.name === ''){
+        return '';
+      }
+      return this.name + ' ' + 'Vasyliev';
+    }
+  },
   methods: {
+    // Only call methods in the view when you want to recalculate it
+    // every time something is changed on the page OR to handle events
     setName(event) {
       this.name = event.target.value;
     },
