371eae87b54ab5d2b687c93fc3217f242748312a
Artur Vasyliev <princeartik@gmail.com>
2021-07-07 20:44:49 +0300

    Basic attack logic

---
+++ the-vuetcher/app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+  data() {
+      return {
+          playerHealth: 100,
+          monsterHealth: 100
+      }
+  },
+  methods: {
+      attackMonster() {
+          const attackValue = getRandomValue(5, 12);
+          this.monsterHealth -= attackValue;
+          this.attackPlayer();
+      },
+      attackPlayer() {
+        const attackValue = getRandomValue(8, 15);
+        this.playerHealth -= attackValue;
+    }
+  }
+});
+
+app.mount("#game");
+
+function getRandomValue(min, max) {
+    // getting random number from min to max
+    return Math.floor(Math.random() * (max - min)) + min;
+}
