b98d863347100d0333a4c279171a3da1b31c643c
Artur Vasyliev <princeartik@gmail.com>
2021-07-08 16:16:30 +0300

    Added basic logic for the app

---
+++ advanced-concepts/vue-cli/second-vue-cli-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "ironman",
+          name: "Tony Stark",
+          phone: "012345 6789",
+          email: "ironman@superhero.com",
+        },
+        {
+          id: "spiderman",
+          name: "Peter Parker",
+          phone: "9745452 6789",
+          email: "spiderman@superhero.com",
+        },
+      ],
+    };
+  },
+};
+</script>
