c09dab5672236bf5d4e62e04b99e70b6aa22adab
Artur Vasyliev <princeartik@gmail.com>
2021-07-08 11:30:45 +0300

    Creating simple test component

--- advanced-concepts/components/intro-components/app.js
+++ advanced-concepts/components/intro-components/app.js
@@ -6,16 +6,49 @@ const app = Vue.createApp({
           id: "ironman",
           name: "Tony Stark",
           phone: "012345 6789",
-          email: "ironman@superhero.com"
+          email: "ironman@superhero.com",
         },
         {
           id: "spiderman",
           name: "Peter Parker",
           phone: "9745452 6789",
-          email: "spiderman@superhero.com"
+          email: "spiderman@superhero.com",
         },
       ],
     };
+  },
+});
+
+// Use multiword id for component to not clash with HTML keywords.
+// Conceptually, component is an app which is connected to another app.
+app.component("friend-contact", {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails()">
+      {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }} </li>
+      <li><strong>Email:</strong> {{ friend.email }} </li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "spiderman",
+        name: "Peter Parker",
+        phone: "9745452 6789",
+        email: "spiderman@superhero.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
   },
 });
 
